ab1b9b5a33d3e9f594160c30bc70efc20d945eab
Sohil Gupta <sohilgupta123@gmail.com>
2018-03-24 16:44:30 +0530

    animation

--- js/init.js
+++ js/init.js
@@ -1,8 +1,23 @@
+window.posts = false;
+$(document).on('click', '.filters .nav-link', function(e) {
+  e.preventDefault();
+  var type = $(this).data('type');
+  if (type !== "all") {
+    $('.post-item').removeClass('animate');
+    $('.post-item').hide();
+    $('.post-item.' + type).show();
+    $('.post-item.' + type).addClass('animate');
+  } else {
+    $('.post-item').show();
+    $('.post-item').addClass('animate');
+  }
+});
 $(document).ready(function() {
+  $('#quick_links').addClass('animate');
+
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     var className = ".nav-background";
-    console.log(scroll);
     if (scroll >= 64) {
       $(className).slideDown('slow');
       $(className).addClass("navbar-fixed");
@@ -10,5 +25,13 @@ $(document).ready(function() {
       $(className).slideUp('slow');
       $(className).removeClass("navbar-fixed");
     }
+    console.log(scroll);
+    if (scroll >= 500 && !window.posts) {
+      window.posts = true;
+      $('.post-item').each(function(index, element) {
+        $(element).addClass('animate');
+      });
+    }
+
   });
 });
